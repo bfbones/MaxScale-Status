8579bf4fbc64777781b18a7f2907a4d7fe389a33
Konrad Mallok <info@konradmallok.de>
2015-06-04 21:37:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MariaDB MaxScale Status</title>
+
+<!-- jQuery -->
+<script type="text/javascript" src="lib/jquery/jquery-2.1.4.min.js"></script>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="lib/bootstrap/css/bootstrap-theme.min.css">
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<h1>MariaDB MaxScale Status</h1>
+
+</body>
+</html>
